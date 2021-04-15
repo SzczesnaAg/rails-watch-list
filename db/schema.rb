886ea061a1bd8348d181e162b0f7aafb4f112ab9
SzczesnaAg <agata.szczesna89@gmail.com>
2021-04-15 13:03:47 +0200

    db created and seeded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_091509) do
+ActiveRecord::Schema.define(version: 2021_04_15_103843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,8 @@ ActiveRecord::Schema.define(version: 2021_04_15_091509) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "overview"
-    t.string "post_url"
-    t.integer "rating"
+    t.string "poster_url"
+    t.float "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
