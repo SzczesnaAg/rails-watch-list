589da51376169c5efa382a84b64acac50803ca44
SzczesnaAg <agata.szczesna89@gmail.com>
2021-04-18 17:47:26 +0200

    styling

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -26,6 +26,7 @@ class ListsController < ApplicationController
   end
 
   def destroy
+    @list = List.find(params[:id])
     @list.destroy
     redirect_to lists_path
   end
@@ -33,6 +34,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
